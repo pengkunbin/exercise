192ddeaeb88981e06969004b9d038348a168a847
jizou <815371297@qq.com>
2019-06-30 13:14:45 +0800

    commit

--- 2019.6.30合并两个有序数组/index.js
+++ 2019.6.30合并两个有序数组/index.js
@@ -4,7 +4,7 @@ const nums2 = [2, 5, 6]
 const n = 3
 
 const merge = function (nums1, m, nums2, n) {
-    for (var i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) {
         nums1[m + i] = nums2[i]
     }
     nums1 = nums1.sort(sortNum)
