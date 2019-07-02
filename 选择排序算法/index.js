fe14f125fafe8e073d6488f54dfb8189e4dca1f4
jizou <815371297@qq.com>
2019-07-02 20:29:00 +0800

    commit

--- 选择排序算法/index.js
+++ 选择排序算法/index.js
@@ -1,7 +1,7 @@
 const arry = [3, 5, 15, 36, 38, 44, 26, 27, 50, 46, 47, 49]
 
 const selectionSort = function (arry) {
-    for (let i = 0; i < arry.length; i++) {
+    for (let i = 0; i < arry.length - 1; i++) {
         let index = i
         for (let j = i + 1; j < arry.length; j++) {
             if (arry[index] > arry[j]) {
