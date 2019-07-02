d47efc5338c2b34b1e1d69d34e64457ee9c480f3
jizou <815371297@qq.com>
2019-07-02 20:27:54 +0800

    commit

--- 插入排序算法/index.js
+++ 插入排序算法/index.js
@@ -1,5 +1,6 @@
 const arry = [3, 5, 15, 36, 38, 44, 26, 27, 50, 46, 47, 49]
 
+//错误写法
 const insertArray = function (arry) {
     for (let i = 1; i < arry.length; i++) {
         for (let j = i - 1; j < arry.length; j++) {
@@ -15,18 +16,18 @@ const insertArray = function (arry) {
 }
 console.log(insertArray(arry))
 
-
+//正确写法
 const insertionSort = function (arr) {
-    const len = arr.length;
-    let preIndex, current;
+    const len = arr.length
+    let preIndex, current
     for (let i = 1; i < len; i++) {
-        preIndex = i - 1;
-        current = arr[i];
+        preIndex = i - 1
+        current = arr[i]
         while (preIndex >= 0 && arr[preIndex] > current) {
-            arr[preIndex + 1] = arr[preIndex];
-            preIndex--;
+            arr[preIndex + 1] = arr[preIndex]
+            preIndex--
         }
-        arr[preIndex + 1] = current;
+        arr[preIndex + 1] = current
     }
-    return arr;
+    return arr
 }
---
+++ 选择排序算法/index.js
@@ -0,0 +1,19 @@
+const arry = [3, 5, 15, 36, 38, 44, 26, 27, 50, 46, 47, 49]
+
+const selectionSort = function (arry) {
+    for (let i = 0; i < arry.length; i++) {
+        let index = i
+        for (let j = i + 1; j < arry.length; j++) {
+            if (arry[index] > arry[j]) {
+                index = j
+            }
+        }
+        let temp = arry[i]
+        arry[i] = arry[index]
+        arry[index] = temp
+        console.log(arry)
+    }
+    return arry
+}
+
+console.log(selectionSort(arry))
