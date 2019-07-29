f1d48d4584d33f5c59ad1ed35a35ad3d3165e8e0
jizou <815371297@qq.com>
2019-07-29 22:43:56 +0800

    init

---
+++ 最小优先队列/index.js
@@ -0,0 +1,51 @@
+class PriorityQueue {
+    constructor(element, priority) {
+        this.items = []
+        this.obj = {
+            'element': element,
+            'priority': priority
+        }
+    }
+
+    enqueue(element, priority) {
+        let obj = {
+            'element': element,
+            'priority': priority
+        }
+        let flag = false
+        if (this.items) {
+            for (let i in this.items) {
+                if (priority < this.items[i].priority) {
+                    this.items.splice(i - 1, 0, obj)
+                    flag = !flag
+                    break
+                }
+            }
+            if (!flag) {
+                this.items.push(obj)
+            }
+        } else {
+            this.items.push(obj)
+        }
+    }
+    dequeue() {
+        return this.items.shift()
+    }
+    front() {
+        return this.items[0]
+    }
+    clear() {
+        this.items = []
+    }
+    print() {
+        for (let i in this.items) {
+            console.log(`data:${this.items[i].element},priority:${this.items[i].priority}`)
+        }
+    }
+}
+
+let demo = new PriorityQueue()
+demo.enqueue('s', 5)
+demo.enqueue('k', 4)
+demo.enqueue('b', 6)
+demo.print()
