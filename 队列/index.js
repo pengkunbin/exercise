194ebcb9ea2b552d5b8ea6fb9e15c1af062aec22
jizou <815371297@qq.com>
2019-07-29 18:41:35 +0800

    init

---
+++ 队列/index.js
@@ -0,0 +1,26 @@
+class Queue {
+    constructor() {
+        this.items = []
+    }
+    enqueue(data) {
+        this.items.push(data)
+    }
+    dequeue() {
+        return this.items.shift()
+    }
+    front() {
+        return this.items[0]
+    }
+    clear() {
+        this.items = []
+    }
+    get size() {
+        return this.items.length
+    }
+    get isEmpty() {
+        return !this.items.length
+    }
+    print() {
+        console.log(this.items.toString())
+    }
+}
