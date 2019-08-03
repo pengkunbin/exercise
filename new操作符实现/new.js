06e0c4fff0a940260a0a851e6fbb0a01c712b4b7
jizou <815371297@qq.com>
2019-08-03 10:50:16 +0800

    init

---
+++ new操作符实现/new.js
@@ -0,0 +1,33 @@
+function newOperator(preobj, ...arg) {
+    let obj = {}
+    if (typeof preobj !== 'function') {
+        throw ' newOperator\'s first param must be a function'
+    }
+
+    newOperator.target = preobj
+
+    obj = Object.create(preobj.prototype)
+
+    //get the parameters passed in
+    const argsArr = [].slice.call(arguments, 1)
+    //return value of the original function & binding this   
+    const preobjReturnResult = preobj.apply(obj, argsArr)
+
+
+    const isObject = typeof ctorReturnResult === 'object' && ctorReturnResult !== null
+    const isFunction = typeof ctorReturnResult === 'function';
+    if (isObject || isFunction) {
+        return ctorReturnResult
+    }
+    return obj
+}
+
+const a = function (a, b) {
+    this.name = 'jizou'
+    let print = function () {
+        console.log('hello jizou')
+    }
+    return a + b
+}
+
+let newObj = newOperator(a, '1', 'string', 1)
