b1730762c9edd390e60e3e937c2b0c3125597a7a
jizou <815371297@qq.com>
2019-08-31 17:56:25 +0800

    init: memory

---
+++ Nodejs内存管理/index.js
@@ -0,0 +1,45 @@
+/**
+ * 单位为字节格式为 MB 输出
+ */
+const format = function (bytes) {
+    return (bytes / 1024 / 1024).toFixed(2) + ' MB';
+};
+
+/**
+ * 封装 print 方法输出内存占用信息 
+ */
+const print = function() {
+    const memoryUsage = process.memoryUsage();
+
+    console.log(JSON.stringify({
+        rss: format(memoryUsage.rss),
+        heapTotal: format(memoryUsage.heapTotal),
+        heapUsed: format(memoryUsage.heapUsed),
+        external: format(memoryUsage.external),
+    }));
+}
+
+
+// example.js
+function Quantity(num) {
+    if (num) {
+        return new Array(num * 1024 * 1024);
+    }
+
+    return num;
+}
+
+function Fruit(name, quantity) {
+    this.name = name
+    this.quantity = new Quantity(quantity)
+}
+
+let apple = new Fruit('apple');
+print();
+let banana = new Fruit('banana', 20);
+print();
+let pear = new Fruit('pear', 20);
+print();
+
+
+
