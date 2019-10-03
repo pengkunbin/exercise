81b2c94218e01f280e347131b18ebe2ca59d238b
jizou <815371297@qq.com>
2019-10-03 20:53:54 +0800

    init

--- 2019.7.8笔试——统计字符个数/index.js
+++
@@ -1,5 +0,0 @@
-const str = 'xyzzyxyz'
-const countStr = function(str){
-    const arry = new Array(str)
-    console.log(arry)
-}
--- LRU算法/index.js
+++ LRU算法/index.js
@@ -1,3 +1,8 @@
 const arry = [2,3,4,5,2,3,8,4,2,6,9]
 const size = 4
-let arry = new Array(4)
+
+
+const LRU = function(arry){
+    let queue = new Array(size)
+    queue
+}
