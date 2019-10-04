0f38b658667d0ffcce68c8a0de4dc71d18b4d49c
jizou <815371297@qq.com>
2019-10-04 10:39:37 +0800

    init

--- 深浅克隆/shallow.js
+++ 深浅克隆/shallow.js
@@ -1,14 +1,17 @@
 //...实现浅克隆
 const person = { name: 'pkb', age: '20' }
 const child = { ...person }
-child.name = 'jizou'
-child.age = '18'
-console.log(person)
 
 //Object.assgin实现浅克隆
-
 const person = { name: 'cxk', singing: 'jinitaimei' }
 const stupid = Object.assign({}, person)
-stupid.singing = 'oh baby baby'
-console.log(person)
-console.log(stupid)
+
+//ES5
+
+const shallowClone = function (obj) {
+    const newObj = {}
+    for (let i in obj) {
+        newObj[i] = obj[i]
+    }
+    return newObj
+}
