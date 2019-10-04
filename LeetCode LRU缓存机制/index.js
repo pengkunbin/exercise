301f560f3c5c2b90a0571a6861f78708a532f901
jizou <815371297@qq.com>
2019-10-04 08:47:36 +0800

    init

---
+++ LeetCode LRU缓存机制/index.js
@@ -0,0 +1,37 @@
+const LRUCache = function (capacity) {
+    this.capacity = capacity
+    this.map = new Map()
+}
+
+LRUCache.prototype.get = function (key) {
+    if (this.map.get(key)) {
+        this.map.delete(key)
+        this.map.set(key, this.map.get(key))
+    } else {
+        return -1
+    }
+}
+
+LRUCache.prototype.put = function (key, value) {
+    if (this.map.has(key)) {
+        this.map.delete(key)
+    }
+    this.map.set(key, value)
+    let keys = this.map.keys()
+    while (this.map.size > this.capacity) {
+        this.map.delete(keys.next().value)
+    }
+}
+
+
+const cache = new LRUCache(2)
+
+cache.put(1, 1)
+cache.put(2, 2)
+cache.get(1)       // 返回  1
+cache.put(3, 3)    // 该操作会使得密钥 2 作废
+cache.get(2)       // 返回 -1 (未找到)
+cache.put(4, 4)    // 该操作会使得密钥 1 作废
+cache.get(1)       // 返回 -1 (未找到)
+cache.get(3)       // 返回  3
+cache.get(4)       // 返回  4
