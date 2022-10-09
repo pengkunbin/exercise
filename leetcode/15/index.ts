63b3e748886dd662615af775b4da20f62fae593a
pengkunbin <815371297@qq.com>
2022-10-09 09:09:51 +0800

    feat(leetcode): 15

--- leetcode/15/index.ts
+++ leetcode/15/index.ts
@@ -1,3 +1,29 @@
 function threeSum(nums: number[]): number[][] {
-    return [[]]
+    const result: number[][] = [];
+    // 拿到数组的长度
+    let n = nums.length;
+    nums.sort((a, b) => a - b);
+
+    for (let i = 0; i < n - 2; i++) {
+        if (nums[i] > 0) {
+            break;
+        }
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        let left = i + 1;
+        let right = n - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+            if (sum == 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+                while (left < right && nums[left] == nums[left + 1]) left++;
+                while (left < right && nums[right] == nums[right - 1]) right--;
+                left++;
+                right--;
+            }
+            else if (sum < 0) left++;
+            else if (sum > 0) right--;
+        }
+    }
+    return result
 };
