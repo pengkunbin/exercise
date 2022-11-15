8ac0734b3153d810178c5a21e662665086016276
pengkunbin <815371297@qq.com>
2022-11-16 01:02:28 +0800

    feat(leetcode): 222

---
+++ leetcode/222/index.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if (root == nullptr) return 0;
+        TreeNode* left = root->left;
+        TreeNode* right = root->right;
+        int leftDepth = 0, rightDepth = 0; // 这里初始为0是有目的的，为了下面求指数方便
+        while (left) {  // 求左子树深度
+            left = left->left;
+            leftDepth++;
+        }
+        while (right) { // 求右子树深度
+            right = right->right;
+            rightDepth++;
+        }
+        if (leftDepth == rightDepth) {
+            return (2 << leftDepth) - 1; // 注意(2<<1) 相当于2^2，所以leftDepth初始为0
+        }
+        return countNodes(root->left) + countNodes(root->right) + 1;
+    }
+};
+
+// 作者：carlsun-2
+// 链接：https://leetcode.cn/problems/count-complete-tree-nodes/solution/by-carlsun-2-bwlp/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
